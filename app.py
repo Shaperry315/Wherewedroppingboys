7b1cab336af2548308df2013aae46d39f60090e1
githubbubber <mekesiabrown@gmail.com>
2018-12-24 18:02:50 -0500

    Add HTTP request module and code, and put results in html results page

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
 	return render_template('index.html')
+
 
 @app.route('/searchresults')
 def searchresults():
@@ -13,9 +15,11 @@ def searchresults():
 	
 	req = requests.get(requestURL, headers={"TRN-Api-Key": "0c4877f8-57bd-411e-b04d-027a6ed9fa85"})
 	res = req.json()
-	print(res)
-	
-	return render_template('searchresults.html', res)
+	# print(res)
+	# res = res["epicUserHandle"]
+	res = [ res["epicUserHandle"], res["stats"]["p2"]["trnRating"]["percentile"] ]
+	return render_template('searchresults.html', res = res)
+
 
 @app.route('/*')
 def home2():
